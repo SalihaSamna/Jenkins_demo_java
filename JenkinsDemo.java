a7ce85425e7964a7e6abdacf69b9c0a188180b53
SalihaSamna <79971697+SalihaSamna@users.noreply.github.com>
2021-03-10 11:09:59 +0530

    Update JenkinsDemo.java

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -1,6 +1,6 @@
 class JenkinsDemo{
 public static void main(String[] args){
 System.out.println("Welcome to jenkins and github integration");
- System.out.println("This is a new commit");
+ System.out.println("This is a new commit using java language");
  }
 }
